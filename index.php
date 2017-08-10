20447b65969b4dd82add83627229cbf9d43a28ef
mattia_zanella <mattia.zanella@link-me.it>
2017-08-10 23:21:41 +0200

    All metadata

--- index.php
+++ index.php
@@ -42,7 +42,9 @@
 				</div>
 				<div class="quote-date m-b-20">
 					<i class="fa fa-calendar" aria-hidden="true"></i>
-					<span id="date">
+					<span
+						id="date"
+						class="m-l-5">
 						<?php echo $mp3file->year; ?>
 					</span>
 				</div>
@@ -69,49 +71,6 @@
 			/*$video = array(
 				array('faccia.mp4','faccia.jpg' ),
 				array('ale.mp4','ale.jpg' ),
-			);
-			$audio = array (
-				array('something_water.mp3','07/01/17','Something in the water' ),
-
-				array('pissio.mp3','04/02/15','Pissio DC' ),
-				array('paul_vs_aleks.mp3','31/01/15','Alla cortese attenzione di Alessandro & Paolo', 'true'),
-				array('vs_aleks.mp3','31/01/15','Alla cortese attenzione di Alessandro & Mattia', 'true'),
-				array('mina_aleks.mp3','01/02/15','Che mina era?' ),
-				array('staff_forte.mp3','01/02/15','STAFF FORTE' ),
-				array('koolDek_emmaMarrone.mp3','08/08/15','Una Padova che non esiste' ),
-				array('dove_sei.mp3','08/08/15','Dove cazzo sei' ),
-				array('vicenza.mp3','11/08/15','Vorrei andare ogno giorno a Vicenza' ),
-				array('nulla.mp3','12/08/15','Nullità' ),
-
-				array('ragionare.mp3','27/09/15','Ragionare' ),
-				array('rash-fibra.mp3','17/10/15','Tutto fibra 5€' ),
-				array('macchina.mp3','04/11/15','Sei in macchina' ),
-				array('ignorante.mp3','05/12/15','Sei ignorante' ),
-				array('mattia.mp3','09/12/15','Oh Mattia Zanella' ),
-				array('star-wars.mp3','16/12/15','Star Wars il risveglio della forza' ),
-				array('ragioneria.mp3','16/12/15','Hai fatto ragioneria' ),
-				array('mi_registrano.mp3','20/12/15','Mi registrano tutti' ),
-				array('libro.mp3','23/12/15','Leggi un libro' ),
-				array('pietro.mp3','09/02/2016','Pietro' ),
-				array('parchetto.mp3','09/02/2016','Party One' ),
-				array('lanciafiamme.mp3','13/02/2016','Lanciafiamme' ),
-				array('merda.mp3','13/02/2016','È una merda' ),
-				array('osa.mp3','21/02/2016','Osa solo' ),
-				array('trammezzo.mp3','21/02/2016','Sei malata' ),
-				array('permesso.mp3','21/02/2016','Autorizzazione' ),
-
-				array('faccia.mp3','21/03/2016','Ha una faccia' ),
-				array('jesolo.mp3','21/03/2016','Se andate a Jesolo' ),
-				array('zane.mp3','21/03/2016','Zane' ),
-				array('vetroloso.mp3','05/04/2016','Vetroloso' ),
-				array('palette.mp3','05/04/2016','Palette colori' ),
-				array('sido.mp3','21/04/2016','2€' ),
-
-				array('keep.mp3','22/12/2016','Keep your fire burning' ),
-				array('vedoNonVedo.mp3','22/12/2016','Vedo non vedo' ),
-
-				array('solo.mp3','28/02/17','Vai da solo' ),
-
 			);*/
 
 		?>
