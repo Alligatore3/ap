a111ce70cdb337b5009946c1cf40e58871be13cf
Mattia Zanella <mattia.zanella@link-me.it>
2018-05-13 20:13:40 +0200

    No more audio playing at the same time

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 $(document).ready( function() {
   // Handler for .ready() called.
+  var allAudios = $( ".track.quote-box" );
+
+  var ifSomebodyElsePlaying = function(clicked){
+    allAudios.each(
+      function(index, element){
+
+        if(
+          $(element).find(".plyr.plyr--audio").hasClass("plyr--playing") &&
+          !($(element).attr("id") === clicked)
+        )
+          $(element).find(".plyr__controls [data-plyr='play']").click();
+      }
+    );
+  };
 
   setTimeout(
     function(){
       $( ".plyr__controls [data-plyr='play']" )
       .click( function() {
-        console.log("1",  $( this ).closest('.quote-box') );
+        var idClickedElement = $(this).closest(".track.quote-box").attr("id")
+        ifSomebodyElsePlaying( idClickedElement );
       })
     },
     2000
